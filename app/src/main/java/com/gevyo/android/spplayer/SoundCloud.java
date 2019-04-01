c672f375767a05ddc4862605c0171ff23433f816
irenegevyo <irenegevyo@gmail.com>
2019-04-01 15:10:49 +0700

    Initial commit

---
+++ app/src/main/java/com/gevyo/android/spplayer/SoundCloud.java
@@ -0,0 +1,16 @@
+package com.gevyo.android.spplayer;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class SoundCloud {
+    private static final Retrofit RETROFIT = new Retrofit.Builder()
+            .baseUrl(Config.API_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+    public static final SCService SERVICE = RETROFIT.create(SCService.class);
+    public static SCService getService() {
+        return SERVICE;
+    }
+}
